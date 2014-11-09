c0fafb5b73e472526e8d3389950b742675bbf1c2
Javier Fonseca <javier.fonseca@blanclink.com>
2014-11-09 11:46:27 -0430

    Rutas con prefijos

--- server.js
+++ server.js
@@ -19,31 +19,35 @@ var configDB = require('./app/config/database.js');
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
+var router = express.Router();
+
 require('./app/config/passport.js')(passport); // pass passport for configuration
 
 app.use(morgan('dev')); // log every request to the console
-    app.use(cookieParser()); // read cookies (needed for auth)
-    app.use(bodyParser.json()); // get information from html forms
-        app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', 'ejs'); // set up ejs for templating
 
     // required for passport
-    app.use(session({
-      secret: 'TheSuperPowerfulSessionKey',
-      resave: true,
-      saveUninitialized: true,
-      cookie: {
-        maxAge: 1800000, //previously set to just 1800 - which was too low
-        httpOnly: true
-      }
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session()); // persistent login sessions
-    app.use(flash()); // use connect-flash for flash messages stored in session
-// routes ======================================================================
-require('./app/routes/user.js')(app, passport); // load our routes and pass in our app and fully configured passport
+app.use(session({
+  secret: 'TheSuperPowerfulSessionKey',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1800000, //previously set to just 1800 - which was too low
+    httpOnly: true
+  }
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
+// routes ======================================================================
+require('./app/routes/user.js')(router, passport); // load our routes and pass in our app and fully configured passport
+
+app.use('/api', router);
 // launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
